b9a50fac84cebf69a3e70fce13c8fc32982cbcf3
Leonardone <neetsdkasu@yahoo.co.uk>
2016-06-02 04:44:18 +0900

    ByteArrayByteIteratorの検査を入れた

--- byteiterator-tester.js
+++ byteiterator-tester.js
@@ -55,4 +55,94 @@ var ByteIteratorTester = new function() {
 		return true;
 	});
 	
+	// ByteArrayByteIterator
+	// -------------------------------------
+	T.makeTest('ByteArrayByteIterator', false, [], function() {
+		log('construct');
+		var iter = new ByteArrayByteIterator();
+		log(iter);
+		log('check common fields');
+		log(common_fields);
+		if (T.checkKey(iter, common_fields) === 0) {
+			return false;
+		}
+		var fields = ['init'];
+		log('check fields');
+		log(fields);
+		if (T.checkKey(iter, fields) === 0) {
+			return false;
+		}
+		log('check first state (before first init)');
+		log('size() 0?');
+		log(iter.size());
+		if (T.check(iter.size() === 0) === 0) {
+			return false;
+		}
+		log('hasNext false?');
+		log(iter.hasNext());
+		if (T.check(iter.hasNext() === false) === 0) {
+			return false;
+		}
+		var test_values = [
+			[[0x10, 0x20, 0x30, 0x40, 0x50, 0x60, 0x70, 0x80, 0x90, 0xa0, 0xb0], 3, 4],
+			[[0x1, 0x2, 0x3, 0x4, 0x5, 0x6, 0x7], 0, 5],
+			[[0xf, 0xe, 0xd, 0xc, 0xb, 0xa, 0x9], 2, 5],
+			[[0x1, 0x2, 0x3], 1, 0],
+			[[], 0, 0]
+		];
+		var i, hasNext_value, next_value;
+		var j, values, offset, len, v;
+		for (j in test_values) {
+			log('case #' + j);
+			v = test_values[j];
+			values = v[0];
+			offset = v[1];
+			len = v[2];
+			log(values);
+			log('offset: ' + offset);
+			log('len: ' + len);
+			iter.init(values, offset, len);
+			log('size ' + len + '?');
+			log(iter.size());
+			if (T.check(iter.size() === len) === 0) {
+				return false;
+			}
+			log('test iterate');
+			for (i = 0; i < len; i++) {
+				log('item: ' + i);
+				log('hasNext true?');
+				hasNext_value = iter.hasNext();
+				log(hasNext_value);
+				if (T.check(hasNext_value === true) === 0) {
+					return;
+				}
+				log('next ' + values[i + offset] + '?');
+				next_value = iter.next();
+				log(next_value);
+				if (T.check(next_value === values[i + offset]) === 0) {
+					return false;
+				}
+			}
+			log('now, must have no item');
+			log('hasNext false?');
+			hasNext_value = iter.hasNext();
+			log(hasNext_value);
+			if (T.check(hasNext_value === false) === 0) {
+				return false;
+			}
+			log('next ' + values[offset + len] + '? (it is reference original array)');
+			next_value = iter.next();
+			log(next_value);
+			if (T.check(next_value === values[offset + len]) === 0) {
+				return false;
+			}
+			log('size ' + len + '? (size must not change)');
+			log(iter.size());
+			if (T.check(iter.size() === len) === 0) {
+				return false;
+			}
+		}
+		return true;
+	});
+	
 };
